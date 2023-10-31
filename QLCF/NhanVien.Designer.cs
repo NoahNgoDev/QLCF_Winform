7c8630646f4dad138e0a221791a9d15e264c634b
Vitamin A <giakhanh220703@gmail.com>
2023-10-31 23:11:40 +0700

    ko có gì

--- QLCF/NhanVien.Designer.cs
+++ QLCF/NhanVien.Designer.cs
@@ -28,8 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // NhanVien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "NhanVien";
+            this.Size = new System.Drawing.Size(1395, 715);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
